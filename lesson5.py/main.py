6367048bd7a873c8308e660dfd009cb9737e8c38
Вадім Пилипяк <vadim@MacBook-Air-Vadim.local>
2025-05-12 16:03:59 +0200

    lesson5.py

---
+++ lesson5.py/main.py
@@ -0,0 +1,53 @@
+#import turtle
+
+#window = turtle.Screen()
+
+#player = turtle.Turtle()
+#player.shape('triangle')
+
+#player.forward(117)
+#player.left(90)
+#player.forward(200)
+#player.left(150)
+#player.forward(220)
+
+#window.mainloop()
+
+
+
+import turtle
+
+window = turtle.Screen()
+
+
+player = turtle.Turtle()
+player.shape('triangle')
+
+
+player.shape('circle')
+player.color('blue')
+player.left(90)
+player.speed(1)
+player.shape('square')
+player.forward(200)
+player.left(90)
+player.color('red')
+player.speed(1)
+player.shape('triangle')
+player.forward(200)
+player.left(90)
+player.speed(1)
+player.color('orange')
+player.shape('turtle')
+player.forward(200)
+player.left(90)
+player.speed(1)
+player.shape('arrow')
+player.color('purple')
+player.forward(200)
+
+
+
+
+
+window.mainloop()
